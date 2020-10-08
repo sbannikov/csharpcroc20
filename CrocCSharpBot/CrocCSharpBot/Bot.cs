0107f7b7c79db486f67c53f0c17f7f0f3a06f32c
Банников Сергей <sbannikov@croc.ru>
2020-10-08 19:55:23 +0300

    Каркас бота.

---
+++ CrocCSharpBot/CrocCSharpBot/Bot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace CrocCSharpBot
+{
+    /// <summary>
+    /// Основной модуль бота
+    /// </summary>
+    public class Bot
+    {
+        /// <summary>
+        /// Клиент Telegram
+        /// </summary>
+        private TelegramBotClient client;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Bot()
+        {
+            // Создание клиента для Telegram
+            client = new TelegramBotClient("1181297092:AAEv84wsLW-sKDuw3JMXpLNR8xC6lJk2HtE");
+            client.OnMessage += MessageProcessor;
+        }
+
+        /// <summary>
+        /// Обработка входящего сообщения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MessageProcessor(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            client.SendTextMessageAsync(e.Message.Chat.Id, "Привет!");
+            Console.WriteLine(e.Message.Text);
+        }
+
+        /// <summary>
+        /// Запуск бота
+        /// </summary>
+        public void Run()
+        {
+            // Запуск приема сообщений
+            client.StartReceiving();
+        }
+    }
+}
---
+++ CrocCSharpBot/CrocCSharpBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CrocCSharpBot
+{
+    /// <summary>
+    /// Главный класс приложения
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Точка входа в приложение
+        /// </summary>
+        /// <param name="args">Параметры командной строки</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск бота в консольном режиме. Нажмите Enter для завершения");
+            Bot bot;
+            bot = new Bot();
+            bot.Run();
+            Console.ReadLine();
+        }
+    }
+}
