745966908de42adbd330fc7539da45a4f575510e
Банников Сергей <sbannikov@croc.ru>
2020-10-29 19:02:51 +0300

    Каркас сервиса

---
+++ CrocCSharpBot/CrocCSharpBot/BotService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrocCSharpBot
+{
+    /// <summary>
+    /// Служба операционной системы
+    /// </summary>
+    partial class BotService : ServiceBase
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public BotService()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Событие запуска сервиса
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Событие останова сервиса
+        /// </summary>
+        protected override void OnStop()
+        {
+        }
+
+        /// <summary>
+        /// Событие приостанова сервиса
+        /// </summary>
+        protected override void OnPause()
+        {
+        }
+
+        /// <summary>
+        /// Событие возобновления сервиса
+        /// </summary>
+        protected override void OnContinue()
+        {
+        }
+    }
+}
--- CrocCSharpBot/CrocCSharpBot/Program.cs
+++ CrocCSharpBot/CrocCSharpBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.ServiceProcess;
 
 namespace CrocCSharpBot
 {
@@ -20,10 +22,31 @@ namespace CrocCSharpBot
         {
             try
             {
-                Bot bot;
-                bot = new Bot();
-                bot.Run();
-                log.Info("Запуск бота в консольном режиме");
+                // Первый параметр командной строки
+                string arg1 = args.Count() > 0 ? args[0] : string.Empty;
+                // Приведение к строчным буквам
+                arg1 = arg1.ToLower();
+
+                switch (arg1)
+                {
+                    case "console":
+                        Bot bot;
+                        bot = new Bot();
+                        bot.Run();
+                        log.Info("Запуск бота в консольном режиме");
+                        break;
+
+                    case "":
+                        var svc = new BotService();
+                        ServiceBase.Run(svc);
+                        break;
+
+                    default: // другой параметр
+                        Console.WriteLine($"Неправильный параметр: {arg1}");
+                        // [!] дописать вывод справки
+                        break;
+                }
+
             }
             catch (Exception ex)
             {
@@ -37,8 +60,11 @@ namespace CrocCSharpBot
             }
             finally
             {
-                Console.WriteLine("Нажмите Enter для завершения");
-                Console.ReadLine();
+                if (Environment.UserInteractive)
+                {
+                    Console.WriteLine("Нажмите Enter для завершения");
+                    Console.ReadLine();
+                }
             }
         }
     }
