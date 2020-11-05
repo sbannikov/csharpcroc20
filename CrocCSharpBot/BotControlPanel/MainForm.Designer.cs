387489b28667a32e6427807dd7322eae1378e971
Банников Сергей <sbannikov@croc.ru>
2020-11-05 19:52:55 +0300

    Утилита управления сервисом

---
+++ CrocCSharpBot/BotControlPanel/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace BotControlPanel
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tool = new System.Windows.Forms.ToolStrip();
+            this.startButton = new System.Windows.Forms.ToolStripButton();
+            this.stopButton = new System.Windows.Forms.ToolStripButton();
+            this.service = new System.ServiceProcess.ServiceController();
+            this.list = new System.Windows.Forms.ListBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.tool.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tool
+            // 
+            this.tool.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.tool.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startButton,
+            this.stopButton});
+            this.tool.Location = new System.Drawing.Point(0, 0);
+            this.tool.Name = "tool";
+            this.tool.Size = new System.Drawing.Size(733, 27);
+            this.tool.TabIndex = 0;
+            this.tool.Text = "toolStrip1";
+            // 
+            // startButton
+            // 
+            this.startButton.Image = ((System.Drawing.Image)(resources.GetObject("startButton.Image")));
+            this.startButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(101, 24);
+            this.startButton.Text = "Запустить";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Image = ((System.Drawing.Image)(resources.GetObject("stopButton.Image")));
+            this.stopButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(114, 24);
+            this.stopButton.Text = "Остановить";
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // service
+            // 
+            this.service.ServiceName = "BotService";
+            // 
+            // list
+            // 
+            this.list.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 16;
+            this.list.Location = new System.Drawing.Point(0, 27);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(733, 425);
+            this.list.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 452);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.tool);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Управление ботом";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tool.ResumeLayout(false);
+            this.tool.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip tool;
+        private System.Windows.Forms.ToolStripButton startButton;
+        private System.Windows.Forms.ToolStripButton stopButton;
+        private System.ServiceProcess.ServiceController service;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ CrocCSharpBot/BotControlPanel/MainForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotControlPanel
+{
+    /// <summary>
+    /// Главная форма приложения
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Конструктор формы
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Запуск сервиса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                service.Start();
+                list.Items.Add("Служба успешно запущена");
+                UpdateButtons();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Останов сервиса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                service.Stop();
+                list.Items.Add("Служба успешно остановлена");
+                UpdateButtons();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Загрузка главной формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                UpdateButtons();
+                timer.Interval = Properties.Settings.Default.TimerIntervalInMilliseconds;
+                timer.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Обновление состояния основных кнопок
+        /// </summary>
+        private void UpdateButtons()
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = true;
+
+            // Обязательное обновление состояния службы
+            service.Refresh();
+            var status = service.Status;
+
+            switch (status)
+            {
+                case System.ServiceProcess.ServiceControllerStatus.Running:
+                    startButton.Enabled = false;
+                    break;
+
+                case System.ServiceProcess.ServiceControllerStatus.Stopped:
+                    stopButton.Enabled = false;
+                    break;
+
+                default: // во всех остальных состояниях управление сервисом невозможно
+                    startButton.Enabled = false;
+                    stopButton.Enabled = false;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Такт таймера
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                timer.Enabled = false;
+                UpdateButtons();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                timer.Enabled = true;
+            }
+        }
+    }
+}
