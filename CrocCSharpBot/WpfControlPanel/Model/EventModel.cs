cc387aa9206ed77747906be04e3fbef8260f0609
Pavel Kondratev <pkondratev@outlook.com>
2020-12-03 20:32:16 +0300

    Список событий

---
+++ CrocCSharpBot/WpfControlPanel/Model/EventModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlPanel.Model
+{
+    public class EventModel
+    {
+        /// <summary>
+        /// Время возникновение события
+        /// </summary>
+        public DateTime EventTime { get; set; }
+
+        /// <summary>
+        /// Сообщение
+        /// </summary>
+        public string Message { get; set; }
+
+        public EventModel(string message, DateTime eventTime)
+        {
+            this.Message = message;
+            this.EventTime = eventTime;
+        }
+
+        public EventModel(string message) : this(message, DateTime.Now) { }
+
+        public EventModel() : this("test message") { }
+    }
+}
---
+++ CrocCSharpBot/WpfControlPanel/ViewModel/EventsVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfControlPanel.Model;
+using System.Windows.Threading;
+using System.Timers;
+
+namespace WpfControlPanel.ViewModel
+{
+    public class EventsVM : ViewModelBase
+    {
+        DateTime RefreshTime { get; set; }
+
+        public ObservableCollection<EventModel> Events { get; set; }
+            = new ObservableCollection<EventModel>();
+
+        public EventsVM(DateTime refreshTime)
+        {
+            this.RefreshTime = refreshTime;
+            Timer = new Timer(RefreshTime.TimeOfDay.TotalMilliseconds);
+            Timer.Elapsed += Timer_Elapsed;
+            Timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(
+        (Action)(() => { Events.Add(new EventModel("My event")); }));
+        }
+
+        Timer Timer { get; set; }
+    }
+}
