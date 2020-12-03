155219bc7ff8c7018f54265209c9a90198725533
Pavel Kondratev <pkondratev@outlook.com>
2020-12-03 18:34:30 +0300

    Приложение на WPF

---
+++ CrocCSharpBot/WpfControlPanel/Model/SettingsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlPanel.Model
+{
+    public class SettingsModel
+    {
+        public DateTime RefreshTimeSpan
+        {
+            get => DateTime.Parse(Properties.Settings.Default.RefreshTimeSpan);
+            set
+            {
+                Properties.Settings.Default.RefreshTimeSpan = value.ToLongTimeString();
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
